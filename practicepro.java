29c88d7929ef9fea11b1ee92c53933b479de4ae9
PMhala <prafullatamhala09@gmail.com>
2025-07-05 22:55:58 +0530

    my first comming

--- practicepro.java
+++ practicepro.java
@@ -4,7 +4,7 @@ public class practicepro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("my first program");
+	System.out.println("my first program has been done");
 
 	}
 
